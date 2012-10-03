39ec475f53979c85f622ec45a23a6b1a1ced564e
Corey Richardson <corey@octayn.net>
2012-10-03 07:18:14 -0400

    Add POSIX time implementation

--- Source/Time.c
+++ Source/Time.c
@@ -2,8 +2,11 @@
 
 #ifdef WINDOWS
 	#include <Windows.h>
+#elif POSIX
+  #include <sys/time.h>
 #endif
 
+/* Return the current time in ms since January 1, 2012 */
 uint64 SAL_Time_Now() {
 	#ifdef WINDOWS
 		FILETIME time;
@@ -14,11 +17,24 @@ uint64 SAL_Time_Now() {
 		result = time.dwHighDateTime;
 		result <<= 32;
 		result += time.dwLowDateTime;
-		result /= 10000; //to shift from 100ns intervals to 1ms intervals
-		result -= 12969849600000; //to shift from epoch of 1/1/1601 to 1/1/2012
+		result /= 10000; // to shift from 100ns intervals to 1ms intervals
+		result -= 12969849600000; // to shift from epoch of 1/1/1601 to 1/1/2012
 
 		return result;
 	#elif POSIX
-		
+		int64 result;
+    struct timeval tv;
+
+    gettimeofday(&tv, null);
+
+    result = time.tv_sec;
+    result *= 1000000; // convert seconds to microseconds
+    result += time.tv_usec; // add microseconds
+    result /= 1000; // convert to milliseconds
+    result -= 1325376000000; // shift from epoch of Jan 1 1970 to Jan 1 2012
+    if (result < 0) {
+      // what do we do in this case?
+    }
+    return (uint64)result;
 	#endif
 }
