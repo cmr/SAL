e092a045769777366b14b1fd28541b67ed1054d5
John <johnarke@live.com>
2012-10-08 20:15:18 -0400

    Removed RandomInt* function prototypes

--- Cryptography.h
+++ Cryptography.h
@@ -12,8 +12,4 @@ public uint32 SAL_Cryptography_RandomUInt32(uint32 floor, uint32 ceiling);
 public uint16 SAL_Cryptography_RandomUInt16(uint16 floor, uint16 ceiling);
 public uint8 SAL_Cryptography_RandomUInt8(uint8 floor, uint8 ceiling);
 
-public int32 SAL_Cryptography_RandomInt32(int32 floor, int32 ceiling);
-public int16 SAL_Cryptography_RandomInt16(int16 floor, int16 ceiling);
-public int8 SAL_Cryptography_RandomInt8(int8 floor, int8 ceiling);
-
 #endif
